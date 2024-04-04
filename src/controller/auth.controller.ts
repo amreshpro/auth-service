604192068fa794b486208dc7b99ae02a20b93f10
Amresh Maurya <amresh.terminal@gmail.com>
2024-04-04 21:20:18 +0530

    add signin controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,10 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+import { NextFunction, Request, Response } from "express";
 import prisma from "../../db/prisma";
 import logger from "../../config/logger";
+// import { Config } from "../../config/index";
 import bcrypt from "bcryptjs";
+import { JwtPayload, sign } from "jsonwebtoken";
+import createHttpError from "http-errors";
 
 class AuthController {
     static async Register(req: Request, res: Response) {
@@ -19,12 +24,11 @@ class AuthController {
                     email,
                     password: hashedPassword,
                 };
+
                 const prismaResponse = await prisma.user.create({
                     data: userData,
                 });
 
-                // eslint-disable-next-line no-console
-                console.log("prismaresponse", prismaResponse);
                 if (prismaResponse) {
                     return res
                         .status(201)
@@ -33,6 +37,84 @@ class AuthController {
             }
             return res.status(500).json({
                 error: "Data Not Saved in our Database",
+            });
+        } catch (error) {
+            logger.error(error);
+            return res.status(500).json({
+                error,
+            });
+        }
+    }
+
+    static async SignIn(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { email, password } = req.body;
+            const tokens = {
+                accessToken: "",
+                refreshToken: "",
+            };
+
+            if (email && password) {
+                const user = await prisma.user.findUnique({
+                    where: email,
+                });
+
+                const payload: JwtPayload = {
+                    sub: String(user?.id),
+                    role: "USER",
+                };
+
+                let privateKey: Buffer;
+                // let publicKey: Buffer;
+
+                try {
+                    privateKey = fs.readFileSync(
+                        path.join(__dirname, "../../certs/private.pem"),
+                    );
+                    // publicKey = fs.readFileSync(
+                    //     path.join(__dirname, "../../certs/public.pem"),
+                    // );
+                } catch (err) {
+                    const error = createHttpError(
+                        500,
+                        "Error while reading private key",
+                    );
+                    next(error);
+                    return;
+                }
+
+                tokens.accessToken = sign(payload, privateKey, {
+                    expiresIn: "1h",
+                    algorithm: "RS256",
+                    issuer: "auth-service",
+                    subject: "",
+                });
+
+                // eslint-disable-next-line no-console
+                console.log("prismaresponse", user);
+                if (user) {
+                    res.cookie("accessToken", tokens.accessToken, {
+                        httpOnly: true, //very important
+                        domain: "localhost",
+                        sameSite: "strict",
+                        maxAge: 1000 * 60 * 60, // 1hr
+                    });
+
+                    res.cookie("refreshToken", tokens.refreshToken, {
+                        httpOnly: true, //very important
+                        domain: "localhost",
+                        sameSite: "strict",
+                        maxAge: 1000 * 60 * 60 * 24 * 365, // 365 dasy
+                    });
+
+                    return res
+                        .status(201)
+                        .json({ msg: "Data saved successfully" });
+                }
+            }
+
+            return res.status(500).json({
+                error: "Email is registered in our Database",
             });
         } catch (error) {
             logger.error(error);
